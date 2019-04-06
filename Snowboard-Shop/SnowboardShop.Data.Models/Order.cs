05b587d473bd6b3041e86015ac01c22d42ad5b0d
Iliyan Antov <i.antov2@gmail.com>
2019-04-06 03:21:24 +0300

    Fixed order placement

--- Snowboard-Shop/SnowboardShop.Data.Models/Order.cs
+++ Snowboard-Shop/SnowboardShop.Data.Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace SnowboardShop.Data.Models {
@@ -7,14 +8,19 @@ namespace SnowboardShop.Data.Models {
 
         public int Id { get; set; }
 
+        [Required]
         public string FirstName { get; set; }
 
+        [Required]
         public string LastName { get; set; }
 
+        [Required]
         public string PhoneNumber { get; set; }
 
+        [Required]
         public string City { get; set; }
 
+        [Required]
         public string Address { get; set; }
 
         public DateTime OrderDate { get; set; }
